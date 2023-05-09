a2bc4874d1a350d2f827e272b8a93de871bff721
MartinezFabian <FabianMartinezDev@gmail.com>
2023-05-09 19:46:57 -0300

    feat: Add functionality to show error message

--- js/app.js
+++ js/app.js
@@ -7,6 +7,7 @@ function main() {
   const inputEmail = document.querySelector("#email");
   const inputSubject = document.querySelector("#subject");
   const inputMessage = document.querySelector("#message");
+  const form = document.querySelector("#form");
 
   //Functions
 
@@ -22,11 +23,46 @@ function main() {
   }
 
   function validateInput(e) {
+    // Si el contenido del input después de eliminar los espacios en blanco está vacío
     if (e.target.value.trim() === "") {
-      // Si el contenido del input después de eliminar los espacios en blanco está vacío
-      console.log("El input esta vació");
+      const parentElement = e.target.parentElement; //obtenemos el div padre del input
+      let message = "";
+
+      //según el id del input obtenemos el mensaje de error
+      switch (e.target.id) {
+        case "email":
+          message = "El campo email es obligatorio";
+          break;
+        case "subject":
+          message = "El campo asunto es obligatorio";
+          break;
+        case "message":
+          message = "El campo mensaje es obligatorio";
+          break;
+      }
+
+      showMessageError(message, parentElement);
     } else {
       console.log("El input tiene información");
     }
+  }
+
+  function showMessageError(message, parentElement) {
+    //comprar si ya existe el mensaje de error en el div padre del input
+    const existMessageError = parentElement.querySelector(
+      ".form__message--error"
+    );
+
+    if (existMessageError) {
+      return;
+    }
+
+    //generar el mensaje de error en el HTML
+    const messageErrorElement = document.createElement("P");
+    messageErrorElement.textContent = message;
+    messageErrorElement.classList.add("form__message", "form__message--error");
+
+    //Agregar el mensaje de error dentro del elemento padre del input
+    parentElement.appendChild(messageErrorElement);
   }
 }
